8f988680bbce0ce76bd258b0adb49af9a0d77f2f
Unknown <Pshangwe@gmail.com>
2021-12-12 17:18:32 +0300

    Site Redo

---
+++ site/pay-per-click/redir/bootstrapcreative.js
@@ -0,0 +1,12 @@
+
+document.addEventListener("DOMContentLoaded", function(event) {
+var htmlString = `<div style="margin:60px auto;text-align:center;">
+<hr>
+<a href="" title="SEO & PPC Management Services in Nairobi"><img src="https://getfitech.com/img/logo.png" alt="Getfitech Consulting Logo" width="350" /></a>
+<br><br>
+<!--<a href="https://getfitech.com" target="_blank">Explore PPC Case Studies</a>&emsp;&emsp;&emsp;&emsp;<a href="https://getfitech.com/store/storefront" target="_blank">Explore Our Advertising Solutions</a>&emsp;&emsp;&emsp;&emsp;<a href="https://getfitech.com/store/faq.html" target="_blank"><strong>What is PPC?</strong></a>-->
+</div>`;
+
+// BootstrapCreative 2018
+$(document.body).append(htmlString);
+});
